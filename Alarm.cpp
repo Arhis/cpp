36dbcb4d9f2733f1896a43a5e5406b433734746b
Arhis <avanture_boy@mail.ru>
2016-09-03 11:30:16 +0300

    Определение и использование констант

---
+++ Alarm.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include <iostream>
+#define SOUND "\a"
+#define NEXT_LINE "\n"
+
+
+int main()
+{
+	using namespace std;
+
+	cout << SOUND << "Don't do that again!" << SOUND << NEXT_LINE;
+	cout << "Here was I! hahahaha";
+
+	cin.get();
+    return 0;
+}
+
